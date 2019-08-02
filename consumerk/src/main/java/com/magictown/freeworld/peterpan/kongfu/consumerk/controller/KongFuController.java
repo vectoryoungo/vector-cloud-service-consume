835440cec2eecaa4414e1daac56e157d6b615423
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-02 17:33:30 +0800

    add controller

---
+++ consumerk/src/main/java/com/magictown/freeworld/peterpan/kongfu/consumerk/controller/KongFuController.java
@@ -0,0 +1,44 @@
+/**
+ * @create 2019-08-02 16:59
+ * @desc kong fu skill
+ **/
+package com.magictown.freeworld.peterpan.kongfu.consumerk.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class KongFuController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+
+    @GetMapping(value = "/load")
+    public String loadKongFu() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("vector-cloud-service");
+        StringBuilder requestContext = new StringBuilder(16);
+        requestContext.append("http://").append(serviceInstance.getHost()).append(":").append(serviceInstance.getPort()).append("getKongFu");
+        System.out.println(" now you are consume " + requestContext.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ParameterizedTypeReference<String> typeReference = new ParameterizedTypeReference<String>() {
+        };
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestContext.toString(), HttpMethod.GET,null,typeReference);
+
+        String result = responseEntity.getBody();
+
+        return result;
+
+    }
+
+}
+
