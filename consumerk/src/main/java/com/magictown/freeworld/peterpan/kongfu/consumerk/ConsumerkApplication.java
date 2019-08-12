9af9af5c7bef7182e5cfe9f14c41f9627d075308
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-12 15:44:13 +0800

    add hystrix

--- consumerk/src/main/java/com/magictown/freeworld/peterpan/kongfu/consumerk/ConsumerkApplication.java
+++ consumerk/src/main/java/com/magictown/freeworld/peterpan/kongfu/consumerk/ConsumerkApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableEurekaClient
 @EnableCaching
+@EnableHystrix
+@EnableHystrixDashboard
 public class ConsumerkApplication {
 
 	public static void main(String[] args) {
